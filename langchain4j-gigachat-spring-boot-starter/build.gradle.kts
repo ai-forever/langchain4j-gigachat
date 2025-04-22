b9266e6391a691f64205fcb80e4f9281877ebc08
Sergey Safonov <ssafonov@sber.ru>
2025-04-22 17:46:12 +0300

    spring disable boot jar

--- langchain4j-gigachat-spring-boot-starter/build.gradle.kts
+++ langchain4j-gigachat-spring-boot-starter/build.gradle.kts
@@ -3,8 +3,16 @@ plugins {
     `java-library`
     id("io.freefair.lombok") version "8.12.2"
     id("langchain4j-gigachat.publish")
-    id("org.springframework.boot") version "3.3.8"
+    id("org.springframework.boot") version "3.4.3"
     id("io.spring.dependency-management") version "1.1.7"
+}
+
+tasks.named<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+    enabled = false
+}
+
+tasks.named<Jar>("jar") {
+    enabled = true
 }
 
 repositories {
@@ -15,7 +23,7 @@ dependencies {
     implementation(project(":langchain4j-gigachat"))
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-autoconfigure-processor")
-
+    implementation("org.springframework.boot:spring-boot-configuration-processor")
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
@@ -26,3 +34,4 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
