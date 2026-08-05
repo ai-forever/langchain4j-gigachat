8055d6e0f62f420f08496c386888770d7f6b13f9
Sergey Safonov <isergeymd@gmail.com>
2026-08-05 14:24:16 +0300

    up gigachat-java:0.1.21

--- langchain4j-gigachat/build.gradle.kts
+++ langchain4j-gigachat/build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 
 dependencies {
     api("dev.langchain4j:langchain4j:1.18.0")
-    api("chat.giga:gigachat-java:0.1.19")
+    api("chat.giga:gigachat-java:0.1.21")
 
     testImplementation(platform("org.junit:junit-bom:5.11.4"))
     testImplementation("org.junit.jupiter:junit-jupiter")
